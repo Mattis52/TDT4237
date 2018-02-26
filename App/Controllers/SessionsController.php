f00a1d800f209f0c80f7d57013c85b3c8d2f5a37
Dora Oline Eriksrud <doraoline@gmail.com>
2018-02-26 22:15:25 +0100

    Working on session lockout after x failed attempts

--- App/Controllers/SessionsController.php
+++ App/Controllers/SessionsController.php
@@ -11,16 +11,15 @@ use \App\System\Auth;
 class SessionsController extends Controller {
 
     public function login() {
-        $_SESSION['failed_attempts'] = 0; // Added, maybe move to where session is created
-        $_SESSION['time_lockout'] = 0;
+
         if(!empty($_POST)) {
             
             $username = isset($_POST['username']) ? $_POST['username'] : '';
             //$password = isset($_POST['password']) ? hash('sha1', Settings::getConfig()['salt'] . $_POST['password']) : '';
             $password = isset($_POST['password']) ? $_POST['password'] : '';
             
-            if($this->auth->checkCredentials($username, $password) and !$_SESSION[time_lockout] = 0) {
-                $failed_attempts = 0; // Added
+            if($this->auth->checkCredentials($username, $password) and !$_SESSION['time_lockout'] = 0) {
+                $_SESSION['failed_attempts'] = 0; // Added
                 setcookie("user", $username);
                 setcookie("password",  $_POST['password']);
                 if ($this->userRep->getAdmin($username)){
@@ -37,12 +36,14 @@ class SessionsController extends Controller {
             }
 
             else {
-              $_SESSION['failed_attempts'] += 1;
+              echo "before: " . $_SESSION['failed_attempts'];
+              $_SESSION['failed_attempts'] = ++$_SESSION['failed_attempts']; // = $_SESSION['failed_attempts'] + 1;
+              echo $_SESSION['failed_attempts'];
               $errors = [
                   "Your username and your password don't match."
               ];
               if( $_SESSION['failed_attempts'] < 3) {
-                array_push($errors, "You have had", $_SESSION['failed_attempts'], " failed logins. After 3, your account will be locked for an increasing timeinvertal, and the admin will be informed.");
+                array_push($errors, "You have had " . $_SESSION['failed_attempts'] . " failed logins. After 3, your account will be locked for an increasing timeinvertal, and the admin will be informed.");
               }
               else if( $_SESSION['failed_attempts'] == 3) {
                 // Warn admin
@@ -50,12 +51,12 @@ class SessionsController extends Controller {
                 $_SESSION['time_lockout'] = true;
                 array_push($errors, "You have had 3 failed logins. Your account is now locked for 30 seconds, and the admin is informed.");
                 // Start time interval
-                increase_session_lockout();
+                $this->increase_session_lockout();
                 $time_lockout = true;
 
               } else if ( $_SESSION['failed_attempts'] >3) {
                 // Increase time interval
-                increase_session_lockout();
+                $this->increase_session_lockout();
                 $time_lockout = true;
               }
             }
@@ -64,7 +65,8 @@ class SessionsController extends Controller {
         $this->render('pages/signin.twig', [
             'title'       => 'Sign in',
             'description' => 'Sign in to the dashboard',
-            'errors'      => isset($errors) ? $errors : ''
+            'errors'      => isset($errors) ? $errors : '',
+            'time_lockout' => isset($time_lockout) ? $time_lockout : false,
         ]);
     }
 
@@ -72,6 +74,15 @@ class SessionsController extends Controller {
         App::redirect();
     }
 
+    // Added NOT WORKING
+    function setInterval($sec) {
+      echo "Setting interval: " .  $sec;
+      while (true) {
+        //sleep($sec);
+        $this->open_account();
+      }
+    }
+    // Added
     private function increase_session_lockout() {
       $new_time;
       if ($_SESSION['time_lockout'] == 0) {
@@ -80,15 +91,20 @@ class SessionsController extends Controller {
       } else {
         $new_time = $_SESSION['time_lockout']**2;
       }
-      $timeout = setInterval($open_account(), $new_time);
+      //$timeout = $this->setInterval($new_time);
     }
 
+    // Added
     private function open_account() {
+      $_SESSION['failed_attempts'] = 0;
       $this->render('pages/signin.twig', [
             'title'       => 'Sign in',
             'description' => 'Sign in to the dashboard',
             'errors'      => isset($errors) ? $errors : ''
         ]);
+
     }
+
+
 
 }
