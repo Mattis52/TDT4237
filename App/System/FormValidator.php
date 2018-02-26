ecdf6d664615138adc866529a1c89df77eab4b0a
Dora Oline Eriksrud <doraoline@gmail.com>
2018-02-26 20:59:42 +0100

    Most password validations are working

--- App/System/FormValidator.php
+++ App/System/FormValidator.php
@@ -26,31 +26,31 @@ class FormValidator {
 
     // Changed this one
     public function validPassword($element, $username, $value, $value_verification) {
-        // Longer than 6 char
-        if (strlen($value) >= 6) {
-            $this->errors[$element] = "The password must be at least 6 characters";
-          // Include number
-        } else if (preg_match('~[0-9]~', $value)) {
+        // At least 7 character        
+        if (strlen($value) < 7) {
+            $this->errors[$element] = "The password must be at least 7 characters";
+        }
+        // Include number
+        else if (!preg_match('~[0-9]~', $value)) {
             $this->errors[$element] = "The password must contain at least one number";
+        } 
         // Include Uppercase
-        }else if (preg_match('~[A-Z]~', $value)) {
+        else if (!preg_match('~[A-Z]~', $value)) {
             $this->errors[$element] = "The password must contain at least one character in uppercase";
           //Not a dictionary word
         // } else if () {
 
           // Numbers and letters are not in sequence
         //} else if () {
-
-          // DOesn't contain username
-
-        //} else if ($value contains $username) {
-        //    $this->errors[$element] = "The password can't contain the username";
-        } else if (empty($value) || ($value != $value_verification)) {
-            echo "First password: " . $value;
-            echo "Second password: " . $value_verification;
-            echo "After second password";
+        } 
+        // Doesn't contain username
+        else if (preg_match('/' . $username . '/', $value)) {
+            $this->errors[$element] = "The password can't contain the username";
+        } 
+        else if (empty($value) || ($value != $value_verification)) {
             $this->errors[$element] = "You didn't write the same password twice";
         }
+        // echo "At the end: " . json_encode($this->errors);
     }
 
     public function validUsername($element, $value, $message) {
