e5ffd290e27f78b50d72bd8fef5eea7ba2572ba3
Dora Oline Eriksrud <doraoline@gmail.com>
2018-02-28 15:12:43 +0100

    Added function to get what env it is running in

--- App/System/Settings.php
+++ App/System/Settings.php
@@ -11,12 +11,22 @@ class Settings {
     public static function getConfig() {
         if(self::$config === null) {
             $config_file  = Yaml::parse((file_get_contents(dirname(__DIR__) . '/config.yml')));
-            self::$environment = $config_file['environment'];
+            // self::$environment = $config_file['environment']; // Commented out
+            $this->$getDev(); // Added
             self::$config = $config_file[self::$environment];
             if(!self::$config) throw new Exception("Config file could not be loaded!");
         }
 
         return self::$config;
+    }
+
+    // Added
+    private static function getDev() {
+       if (isset($_SERVER,$_SERVER['HTTP_HOST']) && $_SERVER['HTTP_HOST'] == 'localhost') {
+        $environment = 'prod';
+       } else {
+        $environemnt = 'dev';
+       }
     }
     
 }
