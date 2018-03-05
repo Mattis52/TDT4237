79a195e6fad3753480ee33036c5fc8d9b1deb65d
Aslak Hollund <aslakhol@gmail.com>
2018-03-05 13:38:27 +0100

    update hash to use SHA-256

--- App/Controllers/SessionsController.php
+++ App/Controllers/SessionsController.php
@@ -12,11 +12,11 @@ class SessionsController extends Controller {
 
     public function login() {
         if(!empty($_POST)) {
-            
+
             $username = isset($_POST['username']) ? $_POST['username'] : '';
-            //$password = isset($_POST['password']) ? hash('sha1', Settings::getConfig()['salt'] . $_POST['password']) : '';
-            $password = isset($_POST['password']) ? $_POST['password'] : '';
-            
+            $password = isset($_POST['password']) ? hash('sha256', Settings::getConfig()['salt'] . $_POST['password']) : '';
+            //$password = isset($_POST['password']) ? $_POST['password'] : '';
+
             if($this->auth->checkCredentials($username, $password)) {
                 setcookie("user", $username);
                 setcookie("password",  $_POST['password']);
